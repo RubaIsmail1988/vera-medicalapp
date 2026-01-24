cb115fbfbf727db9ea40427d8767115754bcb1c0
Ruba Ismail <ruba.hikmat.ismail.com>
2026-01-24 10:21:29 +0300

    Revert "Mobile: update constants for scheduling policy UI"
    This reverts commit 533157864fc7aceebd9ca799521090be00c0dc30.

--- mobile/lib/utils/constants.dart
+++ mobile/lib/utils/constants.dart
@@ -1,5 +1,5 @@
-const String baseUrl = "http://127.0.0.1:8000";
-//const String baseUrl = "https://rubaismail.pythonanywhere.com";
+//const String baseUrl = "http://127.0.0.1:8000";
+const String baseUrl = "https://rubaismail.pythonanywhere.com";
 //const String baseUrl = "http://10.0.2.2:8000";
 //const String baseUrl = "http://192.168.49.101:8000";
 
